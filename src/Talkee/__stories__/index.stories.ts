0eb1c3306ce86e7a1b57b1d7217f98d0a95c4d15
bobby.li <bobby.lhan@gmail.com>
2021-05-20 15:56:52 +0800

    demo

--- src/Talkee/__stories__/index.stories.ts
+++ src/Talkee/__stories__/index.stories.ts
@@ -27,7 +27,7 @@ storiesOf('Talkee', module)
       :siteId="6"
       :slug="1234"
       apiBase="https://talkee-test-api.firesbox.com/api"
-      loginUrl="https://mixin-oauth.firesbox.com/?client_id=65fdd5af-5d3f-496a-9d62-b5cdb4d8396f&scope=PROFILE:READ+PHONE:READ&redirect_url=${redirect_url}"
+      loginUrl="https://mixin-oauth.firesbox.com/?client_id=65fdd5af-5d3f-496a-9d62-b5cdb4d8396f&scope=PROFILE:READ+PHONE:READ&response_type=code&redirect_url=${redirect_url}"
     />`
     }
   })
